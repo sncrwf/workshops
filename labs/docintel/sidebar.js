2be11805e98deca89c08e1714e134a8e168e2798
Lucas Almeida <lucas.b.almeida@live.com>
2024-08-23 16:30:59 -0300

    sidebar change

--- labs/docintel/sidebar.js
+++ labs/docintel/sidebar.js
@@ -7,7 +7,7 @@ const sidebars = {
         'introduction',
         {
             type: 'category',
-            label: '1. Design the solution',
+            label: '1. Desenhe a solução',
             items: [
                 'design/1-locate-doc',
                 'design/2-explore-tables',
@@ -15,7 +15,7 @@ const sidebars = {
         },
         {
             type: 'category',
-            label: '2. Configure a Document Intelligence use case and its integration',
+            label: '2. Configure o caso de uso do Document Intelligence e Integração',
             items: [
                 'configure-docintel/1-use-case',
                 'configure-docintel/2-flow',
@@ -24,7 +24,7 @@ const sidebars = {
         },
         {
             type: 'category',
-            label: '3. End-to-end process',
+            label: '3. Processo End-to-End',
             items: [
                 'e2e/1-submit',
                 'e2e/2-autofill',
